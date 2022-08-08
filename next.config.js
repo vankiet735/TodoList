d3928bb3b1401887c891cc2d7ac41cc4055fd7d5
vankiet735 <vankiet.735@gmail.com>
2022-08-08 19:02:25 +0700

    Update first-time

--- next.config.js
+++ next.config.js
@@ -1,7 +1,51 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+  
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const withAntdLess = require('next-plugin-antd-less');
+
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
+
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './public/antd-custom.less'), 'utf8')
+)
+
+
+
+module.exports = withAntdLess({
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
+  // modifyVars: {
+  //   '@primary-color': 'red',
+  //    '@btn-danger-color': 'red',
+  //    '@link-color': '#faad14',
+  //    '@error-color': '#1890ff'
+  // },
+  // optional: you can modify antd less variables directly here
+  // modifyVars: { '@primary-color': '#f5222d', '@danger-color': '#52c41a' },
+  // Or better still you can specify a path to a file 
+  modifyVars: themeVariables,
+  // lessVarsFilePath: './styles/variables.less',
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config; 
+  }
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+require("../styles/variables.less");
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+ 
